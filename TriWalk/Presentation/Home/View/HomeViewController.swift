399b9bd3bcf0db6693c9bfd944ff113016a5e7bb
SeBin-Kwon <sbkwon98@gmail.com>
2025-03-28 01:18:09 +0900

    feat: Home화면 파일 추가

---
+++ TriWalk/Presentation/Home/View/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  TriWalk
+//
+//  Created by Sebin Kwon on 3/28/25.
+//
+
+import UIKit
+import Combine
+import SnapKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
---
+++ TriWalk/Presentation/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewModel.swift
+//  TriWalk
+//
+//  Created by Sebin Kwon on 3/28/25.
+//
+
+import Foundation
+import Combine
+
+final class HomeViewModel: BaseViewModel, ViewModelType {
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        
+        return Output()
+    }
+}
