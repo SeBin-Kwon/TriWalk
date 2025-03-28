9068c00def7006b1785490ca86a17586c2b1861f
SeBin-Kwon <sbkwon98@gmail.com>
2025-03-28 16:04:26 +0900

    feat: 익스텐션 및 컴포넌트 파일 생성

---
+++ TriWalk/Core/Extensions/UIImage+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIImage+Extension.swift
+//  TriWalk
+//
+//  Created by Sebin Kwon on 3/28/25.
+//
+
+import Foundation
---
+++ TriWalk/Resources/DesignSystem/SFSymbol.swift
@@ -0,0 +1,8 @@
+//
+//  SFSymbol.swift
+//  TriWalk
+//
+//  Created by Sebin Kwon on 3/28/25.
+//
+
+import Foundation
