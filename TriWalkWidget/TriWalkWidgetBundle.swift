0c22ac4402a13ad3a463bd21220baae1d5ddfee0
SeBin-Kwon <sbkwon98@gmail.com>
2025-04-22 18:24:15 +0900

    feat: Widget 추가

---
+++ TriWalkWidget/TriWalkWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TriWalkWidgetBundle.swift
+//  TriWalkWidget
+//
+//  Created by Sebin Kwon on 4/22/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TriWalkWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TriWalkWidget()
+    }
+}
