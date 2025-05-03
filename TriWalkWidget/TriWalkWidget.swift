9ddc12097d892f2e12b073aaf2b25ae62566dea8
SeBin-Kwon <sbkwon98@gmail.com>
2025-05-03 23:48:35 +0900

    feat: widget model 구성

--- TriWalkWidget/TriWalkWidget.swift
+++ TriWalkWidget/TriWalkWidget.swift
@@ -9,23 +9,39 @@ import WidgetKit
 import SwiftUI
 
 struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), emoji: "😀")
+    func placeholder(in context: Context) -> WalkTodaySummaryEntry {
+        WalkTodaySummaryEntry(
+            date: Date(),
+            summary: WalkTodaySummary(date: Date(), distance: 0.0, steps: 0, duration: 0, isWalkToday: false)
+        )
+        
+//        SimpleEntry(date: Date(), emoji: "😀")
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), emoji: "😀")
+    func getSnapshot(in context: Context, completion: @escaping (WalkTodaySummaryEntry) -> ()) {
+        let entry = WalkTodaySummaryEntry(
+            date: Date(),
+            summary: WalkTodaySummary(date: Date(), distance: 2.5, steps: 3500, duration: 1800, isWalkToday: true)
+        )
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
+        var entries: [WalkTodaySummaryEntry] = []
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, emoji: "😀")
+            
+            // MARK: 데이터 가져오기
+            
+            let entry = WalkTodaySummaryEntry(
+                date: Date(),
+                summary: WalkTodaySummary(date: Date(), distance: 2.5, steps: 3500, duration: 1800, isWalkToday: true)
+            )
+            
+            
             entries.append(entry)
         }
 
@@ -38,21 +54,35 @@ struct Provider: TimelineProvider {
 //    }
 }
 
-struct SimpleEntry: TimelineEntry {
+struct WalkTodaySummary {
     let date: Date
-    let emoji: String
+    let distance: Double
+    let steps: Int
+    let duration: TimeInterval
+    let isWalkToday: Bool
 }
+
+struct WalkTodaySummaryEntry: TimelineEntry {
+    let date: Date
+    let summary: WalkTodaySummary
+}
+
+
+//struct SimpleEntry: TimelineEntry {
+//    let date: Date
+//    let emoji: String
+//}
 
 struct TriWalkWidgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
         VStack {
-            Text("Time:")
             Text(entry.date, style: .time)
-
-            Text("Emoji:")
-            Text(entry.emoji)
+                .bold()
+            Text("거리: \(entry.summary.distance)")
+            Text("걸음수: \(entry.summary.steps)")
+            Text("시간: \(entry.summary.duration)")
         }
     }
 }
@@ -71,14 +101,18 @@ struct TriWalkWidget: Widget {
                     .background()
             }
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("오늘의 산책 여행")
+        .description("오늘의 산책 여행 기록을 확인해 보세요.")
+        .supportedFamilies([.systemSmall])
     }
 }
 
 #Preview(as: .systemSmall) {
     TriWalkWidget()
 } timeline: {
-    SimpleEntry(date: .now, emoji: "😀")
-    SimpleEntry(date: .now, emoji: "🤩")
+    WalkTodaySummaryEntry(
+        date: Date(),
+        summary: WalkTodaySummary(date: Date(), distance: 2.5, steps: 3500, duration: 1800, isWalkToday: true)
+    )
+//    SimpleEntry(date: .now, emoji: "🤩")
 }
